fa45b7c6eb5da42bae4c3a0f90cdd0a547edd906
Rodrigo Oliveira <artilheiro@yahoo.com>
2025-04-26 17:26:16 -0700

    Laxed session settings

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ let sessionConfig = {
     maxAge: 1000 * 60 * 60 * 24, // 1 day
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict'
+    sameSite: process.env.NODE_ENV === 'production' ? 'lax' : 'strict'
   }
 };
 
