5a8de8629f60d477dfe40ae5d041cdf8021469e0
Adrian Aquino <aquinoa1103@gmail.com>
2025-05-14 11:39:04 -0700

    Add image upload functionality to recipe controller

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,130 +1,251 @@
 /**
  *
  * @author - Emma Wright, Adrian Aquino
- * @file recipeController.js - Recipe search function
+ * @file recipeController.js - Recipe controller functions
  *
  * 5/13/25 - Modified by Adrian Aquino, added search function
  * 5/14/25 - Modified by Adrian Aquino, added recipe deletion
+ * 5/14/25 - Modified by Adrian Aquino, added image upload functionality
  */
 
-//creating a recipe (emma)
-
 const Recipe = require('../models/Recipe');
+const RecipeImage = require('../models/RecipeImage');
+const upload = require('../middlewares/upload');
 
+/**
+ *
+ * getRecipeImage
+ *
+ * Retrieves an image for a recipe
+ *
+ * @param    req    HTTP request object
+ * @param    res    HTTP response object
+ * @param    next   Next middleware function
+ *
+ */
+exports.getRecipeImage = async (req, res, next) => {
+  try {
+    const recipeId = req.params.id;
+
+    // Find image in database
+    const image = await RecipeImage.findOne({ recipeId });
+
+    if (!image || !image.data) {
+      return res.status(404).send('Image not found');
+    }
+
+    // Set the content type header and send the image data
+    res.set('Content-Type', image.contentType);
+    res.send(image.data);
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ *
+ * createRecipe
+ *
+ * Creates a new recipe with possible image
+ *
+ * @param    req    HTTP request object
+ * @param    res    HTTP response object
+ * @param    next   Next middleware function
+ *
+ */
 exports.createRecipe = async (req, res, next) => {
   try {
-    const {
-      title,
-      description,
-      prepTime,
-      cookTime,
-      estimatedCost,
-      category,
-      instructions,
-    } = req.body;
+    upload.single('recipeImage')(req, res, async (err) => {
+      if (err) {
+        return res.status(400).render('user/recipe-form', {
+          title: 'Create Recipe',
+          errors: [{ msg: err.message || 'File upload error' }],
+          recipe: null
+        });
+      }
 
-    const parsedInstructions = instructions.split('\n').map(s => s.trim()).filter(Boolean);
+      try {
+        const {
+          title,
+          description,
+          prepTime,
+          cookTime,
+          estimatedCost,
+          category,
+          instructions,
+        } = req.body;
 
-    // Ingredients
-    let parsedIngredients;
-    if (Array.isArray(req.body.ingredients)) {
-      parsedIngredients = req.body.ingredients.map((ing) => ({
-        name: ing.name,
-        quantity: ing.quantity
-      }));
-    } else if (typeof req.body.ingredients === 'string') {
-      parsedIngredients = req.body.ingredients.split('\n').map(line => {
-        const [name, quantity] = line.split(':');
-        return { name: name?.trim(), quantity: quantity?.trim() || '' };
-      });
-    }
+        const parsedInstructions = instructions.split('\n').map(s => s.trim()).filter(Boolean);
 
-    const dietaryRestrictions = {
-      vegetarian: !!req.body.dietaryRestrictions?.vegetarian,
-      vegan: !!req.body.dietaryRestrictions?.vegan,
-      glutenFree: !!req.body.dietaryRestrictions?.glutenFree,
-      dairyFree: !!req.body.dietaryRestrictions?.dairyFree,
-      nutFree: !!req.body.dietaryRestrictions?.nutFree
-    };
+        // Ingredients
+        let parsedIngredients;
+        if (Array.isArray(req.body.ingredients)) {
+          parsedIngredients = req.body.ingredients.map((ing) => ({
+            name: ing.name,
+            quantity: ing.quantity
+          }));
+        } else if (typeof req.body.ingredients === 'string') {
+          parsedIngredients = req.body.ingredients.split('\n').map(line => {
+            const [name, quantity] = line.split(':');
+            return { name: name?.trim(), quantity: quantity?.trim() || '' };
+          });
+        }
 
-    const newRecipe = new Recipe({
-      title,
-      description,
-      ingredients: parsedIngredients,
-      instructions: parsedInstructions,
-      prepTime,
-      cookTime,
-      estimatedCost,
-      category,
-      dietaryRestrictions,
-      createdBy: req.session.user.id
+        const dietaryRestrictions = {
+          vegetarian: !!req.body.dietaryRestrictions?.vegetarian,
+          vegan: !!req.body.dietaryRestrictions?.vegan,
+          glutenFree: !!req.body.dietaryRestrictions?.glutenFree,
+          dairyFree: !!req.body.dietaryRestrictions?.dairyFree,
+          nutFree: !!req.body.dietaryRestrictions?.nutFree
+        };
+
+        // Set hasImage based on whether an image was uploaded
+        const hasImage = !!req.file;
+
+        const newRecipe = new Recipe({
+          title,
+          description,
+          ingredients: parsedIngredients,
+          instructions: parsedInstructions,
+          prepTime,
+          cookTime,
+          estimatedCost,
+          category,
+          dietaryRestrictions,
+          createdBy: req.session.user.id,
+          hasImage
+        });
+
+        const savedRecipe = await newRecipe.save();
+
+        // If image was uploaded, save it
+        if (req.file) {
+          const newImage = new RecipeImage({
+            recipeId: savedRecipe._id,
+            data: req.file.buffer,
+            contentType: req.file.mimetype
+          });
+          await newImage.save();
+        }
+
+        res.redirect('/user/profile');
+      } catch (err) {
+        next(err);
+      }
     });
-
-    await newRecipe.save();
-
-    res.redirect('/user/profile');
   } catch (err) {
     next(err);
   }
 };
 
-//editing a recipe 
-
+/**
+ *
+ * updateRecipe
+ *
+ * Updates a recipe with possible new image
+ *
+ * @param    req    HTTP request object
+ * @param    res    HTTP response object
+ * @param    next   Next middleware function
+ *
+ */
 exports.updateRecipe = async (req, res, next) => {
   try {
-    const {
-      title,
-      description,
-      prepTime,
-      cookTime,
-      estimatedCost,
-      category,
-      instructions,
-    } = req.body;
+    upload.single('recipeImage')(req, res, async (err) => {
+      if (err) {
+        const recipe = await Recipe.findById(req.params.id);
+        return res.status(400).render('user/recipe-form', {
+          title: 'Edit Recipe',
+          errors: [{ msg: err.message || 'File upload error' }],
+          recipe
+        });
+      }
 
-    const parsedInstructions = instructions.split('\n').map(s => s.trim()).filter(Boolean);
+      try {
+        const {
+          title,
+          description,
+          prepTime,
+          cookTime,
+          estimatedCost,
+          category,
+          instructions,
+        } = req.body;
 
-    // Ingredients
-    let parsedIngredients;
-    if (Array.isArray(req.body.ingredients)) {
-      parsedIngredients = req.body.ingredients.map((ing) => ({
-        name: ing.name,
-        quantity: ing.quantity
-      }));
-    } else if (typeof req.body.ingredients === 'string') {
-      parsedIngredients = req.body.ingredients.split('\n').map(line => {
-        const [name, quantity] = line.split(':');
-        return { name: name?.trim(), quantity: quantity?.trim() || '' };
-      });
-    }
+        const parsedInstructions = instructions.split('\n').map(s => s.trim()).filter(Boolean);
 
-    const dietary = req.body.dietaryRestrictions || {};
-    const dietaryRestrictions = {
-      vegetarian: !!dietary.vegetarian,
-      vegan: !!dietary.vegan,
-      glutenFree: !!dietary.glutenFree,
-      dairyFree: !!dietary.dairyFree,
-      nutFree: !!dietary.nutFree
-    };
+        // Ingredients
+        let parsedIngredients;
+        if (Array.isArray(req.body.ingredients)) {
+          parsedIngredients = req.body.ingredients.map((ing) => ({
+            name: ing.name,
+            quantity: ing.quantity
+          }));
+        } else if (typeof req.body.ingredients === 'string') {
+          parsedIngredients = req.body.ingredients.split('\n').map(line => {
+            const [name, quantity] = line.split(':');
+            return { name: name?.trim(), quantity: quantity?.trim() || '' };
+          });
+        }
 
-    await Recipe.findByIdAndUpdate(req.params.id, {
-      title,
-      description,
-      ingredients: parsedIngredients,
-      instructions: parsedInstructions,
-      prepTime,
-      cookTime,
-      estimatedCost,
-      category,
-      dietaryRestrictions
+        const dietary = req.body.dietaryRestrictions || {};
+        const dietaryRestrictions = {
+          vegetarian: !!dietary.vegetarian,
+          vegan: !!dietary.vegan,
+          glutenFree: !!dietary.glutenFree,
+          dairyFree: !!dietary.dairyFree,
+          nutFree: !!dietary.nutFree
+        };
+
+        const updateData = {
+          title,
+          description,
+          ingredients: parsedIngredients,
+          instructions: parsedInstructions,
+          prepTime,
+          cookTime,
+          estimatedCost,
+          category,
+          dietaryRestrictions
+        };
+
+        // If a new image was uploaded, update the hasImage flag
+        if (req.file) {
+          updateData.hasImage = true;
+        }
+
+        const recipe = await Recipe.findByIdAndUpdate(req.params.id, updateData, { new: true });
+
+        // If image was uploaded, save it
+        if (req.file) {
+          // Check if recipe already has an image
+          const existingImage = await RecipeImage.findOne({ recipeId: req.params.id });
+
+          if (existingImage) {
+            // Update existing image
+            existingImage.data = req.file.buffer;
+            existingImage.contentType = req.file.mimetype;
+            await existingImage.save();
+          } else {
+            // Create new image document
+            const newImage = new RecipeImage({
+              recipeId: req.params.id,
+              data: req.file.buffer,
+              contentType: req.file.mimetype
+            });
+            await newImage.save();
+          }
+        }
+
+        res.redirect('/user/profile');
+      } catch (err) {
+        next(err);
+      }
     });
-
-    res.redirect('/user/profile');
   } catch (err) {
     next(err);
   }
 };
-
 
 //added by Adrian Aquino
 
