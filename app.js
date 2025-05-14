41ca1fdb205d09735871ec915b886c2e3a1d70f2
LaxRaj <lakshayraj.00100@gmail.com>
2025-05-14 16:24:50 -0700

    final polish -lakshya

--- app.js
+++ app.js
@@ -168,7 +168,7 @@ app.use('/recipe', recipeRoutes);
 app.use(handleErrors);
 
 // Start server
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
